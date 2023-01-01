b382a4b088b670eab91d64cf80db871ca4452130
Orphsy <bariscum07@gmail.com>
2023-01-01 23:33:59 +0300

    Rabits and Carrots Removed

--- BlazorApp1/Data/Environment.cs
+++ BlazorApp1/Data/Environment.cs
@@ -22,8 +22,7 @@ namespace BlazorApp1.Data
         }
         public int total_of_cols() {
             return this.cols;
-        }
-        
+        }        
         private bool rightPos(int i,int j){
             return i>=0 && i<this.rows && j>=0 && j<this.cols;
         }
@@ -34,7 +33,6 @@ namespace BlazorApp1.Data
                 this.cell[i, j] = been;
             }
         }
-
         public void onOff(int row, int col)
         {
             if (this.rightPos(row, col))
@@ -45,7 +43,6 @@ namespace BlazorApp1.Data
                     this.cell[row, col] = new BioUnit(row, col, this);
             }
         }
-
         public BioUnit biounit(int i, int j)
         {
             if (this.rightPos(i, j))
@@ -54,7 +51,6 @@ namespace BlazorApp1.Data
             }
             return null;
         }
-
         public List<BioUnit>neighbors(int i,int j)
         {
             List<BioUnit> ans = new List<BioUnit>();
@@ -72,7 +68,6 @@ namespace BlazorApp1.Data
             }
             return ans;
         }
-
         public int surrondingNeighborns(int i,int j, String specie)
         {
             int ans = 0;
@@ -84,7 +79,6 @@ namespace BlazorApp1.Data
             }
             return ans;
         }
-
         public string specie(Object obj)
         {
             String[] w;
@@ -92,65 +86,6 @@ namespace BlazorApp1.Data
             w = TypeDescriptor.GetClassName(obj).Split(".");
             return w[w.Length - 1];
         }
-
-        public Rabbit firstRabbit(int i, int j)
-        {
-            List<BioUnit> neis = this.neighbors(i, j);
-            foreach (object unit in neis )
-            {
-                if (this.specie(unit) == "Rabbit")
-                    return (Rabbit)unit;
-            }
-            return null;
-        }
-
-        public void next_Rabbit_Carrot_Step()
-        {
-            BioUnit[,] aux = new BioUnit[this.rows, this.cols];
-            for(var i=0; i<this.rows; i++)
-            for(var j=0; j<this.cols; j++)
-                {
-                    aux[i, j] = null;
-                    if (this.specie(this.cell[i, j]) == "Carrot")
-                    {
-                        if (this.surrondingNeighborns(i, j, "Rabbit") == 0)
-                        {
-                            if (this.cell[i, j].will_I_live())
-                                aux[i, j] = this.cell[i, j];
-                        }
-                        else
-                        {
-                            this.firstRabbit(i, j).eat();
-                        }
-                    }
-                    else if (this.specie(this.cell[i, j]) == "Rabbit")
-                    {
-                        if (this.cell[i, j].will_I_live())
-                        {
-                            aux[i, j] = this.cell[i, j];
-                        }
-                    }
-                    else
-                    {
-                        if (this.cell[i, j] == null)
-                        {
-                            if (this.surrondingNeighborns(i, j, "Rabbit")>=2)
-                            {
-                                aux[i, j] = new Rabbit(i, j, this);
-                            }
-                            else if(this.surrondingNeighborns(i, j, "Carrot") >= 3)
-                            {
-                                aux[i, j] = new Carrot(i, j, this);
-                            }
-                        }
-                    }
-                }
-                for (var i = 0; i < this.rows; i++)
-                for (var j = 0; j < this.cols; j++){
-                    this.cell[i, j] = aux[i, j];
-           }
-        }
-
         public void nextConwayStep()
         {
             int n;
@@ -175,7 +110,6 @@ namespace BlazorApp1.Data
                         this.cell[i, j] = null;
                 }
         }
-
         public void put_pattern(int x,int y, string pattern)
         {
             if (pattern.Equals("pentadecathlon"))
